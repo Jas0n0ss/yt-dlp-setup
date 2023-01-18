b4e550a04b962e88a09522ee726f2018d08634b4
Jas0n0ss <88020021+Jas0n0ss@users.noreply.github.com>
2023-01-19 01:35:35 +0800

    Update tubedownloader.sh
    Some improvement

--- script/tubedownloader.sh
+++ script/tubedownloader.sh
@@ -13,15 +13,23 @@ echo  "All Available 2K above mp4 format list: " | lolcat
 echo  "#################################################" | lolcat
 yt-dlp -F $1 | grep -E  "^399|^137|^400|^401" | lolcat
 echo  "#################################################" | lolcat
+echo  "All Available mpa audio format list: " | lolcat
+echo  "#################################################" | lolcat
+yt-dlp -F $1 | grep -w m4a |lolcat
 echo "Vide ID Please (The first row of the Number), leave it blank will download 4K mp4:" |lolcat
-read -r QUA
+read -r vID
+VID=${vID:-"401"}
+echo "Audio ID Please (The first row of the Number), leave it blank will download best m4a:" |lolcat
+read -r aID
+AID=${aID:-"140"}
+QUA="${VID}+${AID}"
+
 echo  "Please enter the Path you want to save the video:" | lolcat
 read -r V_PATH
 # Best quality By default
-Quality=${QUA:-"4k"}
-
-if [ $Quality == "4k" ];then
-    yt-dlp -f 401 -P $V_PATH --restrict-filenames $1 | lolcat && echo "Successful downloaded to $V_PATH" | lolcat
-else
-    yt-dlp -f $QUA -P $V_PATH --restrict-filenames $1 | lolcat  && echo "Successful downloaded to $V_PATH" | lolcat
-fi
+yt-dlp -f $QUA -P $V_PATH --restrict-filenames $1 | lolcat
+echo  "#################################################" | lolcat
+echo  "#################################################" | lolcat
+lolcat && echo "Successful downloaded to $V_PATH" | lolcat
+echo  "#################################################" | lolcat
+echo  "#################################################" | lolcat
